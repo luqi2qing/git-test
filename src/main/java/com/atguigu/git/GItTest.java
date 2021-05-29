0956cffdb45ec2e0e2d27989a401b889a1841482
qingpingyue@talkweb.com.cn <1224825260@qq.com>
2021-05-29 22:31:42 +0800

    third commit

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -16,5 +16,8 @@ public class GItTest {
         System.out.println("gitee test!");
         System.out.println("gitee test2!");
         System.out.println("gitee test23!");
+        System.out.println("gitee test23!");
+        System.out.println("gitee test23!");
+        System.out.println("gitee test23!");
     }
 }
